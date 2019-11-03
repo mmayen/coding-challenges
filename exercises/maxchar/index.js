43126d355b81665f5e81ed510c3bcd588d9fefd4
Mmayen Ewang <mmayenewang@Mmayens-MacBook-Pro.local>
2019-11-02 20:44:26 -0400

    implement maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+    const charMap = {};//Caharcter map
+    let max = 0;
+    let maxChar ='';
+    //populate map with char-frequency pairs for each character in str
+    for(let char of str){
+        charMap[char] = charMap[char]+ 1 || 1;
+    }
+    //check for max frequency 
+    for(let char in charMap){
+        if(charMap[char] > max){
+            max =charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar
+}
 module.exports = maxChar;
