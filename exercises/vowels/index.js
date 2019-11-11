c314bebc8e2524f9390feacd2247109dacb36ba7
Mmayen Ewang <mmayenewang@Mmayens-MacBook-Pro.local>
2019-11-10 21:15:34 -0500

    implements vowel check

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels(str) {
+    let count = 0;
+    let vowelArray = ['a', 'e', 'i', 'o', 'u'];
+    
+    for(let char of str.toLowerCase()){
+        if(vowelArray.includes(char)){
+            count++;
+        }
+    }
+    return count;
+    /* Second Solution below
+    let matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+    */
+}
+console.log(vowels("MMAYen is a good girl"));
 module.exports = vowels;
