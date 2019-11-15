b70bbfd313dfaffbecb8bf4f0eb2b95f41b61798
Mmayen Ewang <mmayenewang@Mmayens-MacBook-Pro.local>
2019-11-14 22:09:26 -0500

    implement node and linkedlist class

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,35 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+    
+    size() {
+        let count = 0;
+        let currNode = this.head;
+        while(currNode) {
+            count++;
+            currNode = currNode.next;
+        }
+        return count;
+    }
+
+    getFirst() {
+        
+    }
+}
 
 module.exports = { Node, LinkedList };
