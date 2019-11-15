aef7a7c2e9119cd91b9b1ebf30e77e66f4a7608a
Mmayen Ewang <mmayenewang@Mmayens-MacBook-Pro.local>
2019-11-14 19:44:20 -0500

    implements a weave Queue form two queues

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,17 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+    const sourceThree = new Queue();
+    while(sourceOne.peek() || sourceTwo.peek()){
+        if(sourceOne.peek()){
+            sourceThree.add(sourceOne.remove());
+        }
+        if(sourceTwo.peek()){
+            sourceThree.add(sourceTwo.remove());
+        }
+    }
+    return sourceThree;
+}
 
 module.exports = weave;
