bfd99a1362fd2ca2491834efcfa9c7d37899138b
Mmayen Ewang <mmayenewang@Mmayens-MacBook-Pro.local>
2019-12-10 21:19:54 -0500

    calculates level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,7 +10,24 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+const Node = require('./node');
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const widthArr = [0];
+    const holdingArr = [root, 's'];
+
+    while(holdingArr.length > 1) {
+        let currNode = holdingArr.shift();
+
+        if(currNode === 's') {
+            widthArr.push(0);
+            holdingArr.push('s');
+        } else {
+            widthArr[widthArr.length - 1]++;
+            holdingArr.push(...currNode.children);
+        }   
+    }
+    return widthArr;
+}
 
 module.exports = levelWidth;
